24b27f0af197df21d0d35f0c35f83f5e91052a31
LEO <hick317@gmail.com>
2023-08-09 23:07:27 +0530

    database link scure

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-const DB = process.env.value;
+const DB = `mongodb+srv://${process.env.password}:${process.env.cpassword}@cluster1.svyukxb.mongodb.net/mernstack`
 mongoose.connect(DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
